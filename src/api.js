a0f75a699cbd9535f1593964d673e6f52f92b8ad
nwils000 <nwils000@gmail.com>
2024-06-07 08:07:19 -0400

    pointing to fly deploy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://127.0.0.1:8000';
-// const baseUrl = 'https://app-social-media.fly.dev/';
+// const baseUrl = 'http://127.0.0.1:8000';
+const baseUrl = 'https://app-social-media.fly.dev/';
 
 export const getToken = async ({ profile, username, password }) => {
   try {
